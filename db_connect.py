46082bbc54fdbe05b280ac995714f577d7146a7b
erensahan99 <eren.sahan99@gmail.com>
2019-08-08 06:02:31 +0300

    database config

--- db_connect.py
+++ db_connect.py
@@ -2,9 +2,9 @@
 import mysql.connector
 
 def connection():
-    conn = mysql.connector.connect(host="localhost",
-                            user = "root",
-                            passwd= "root",
+    conn = mysql.connector.connect(host="mysql://b70af944e93ac1:e2aed107@us-cdbr-iron-east-02.cleardb.net/heroku_78cf1cf050b4c37?reconnect=true",
+                            user = "b70af944e93ac1",
+                            passwd= "e2aed107",
                             db = "renta")
     c = conn.cursor()
     return c,conn
