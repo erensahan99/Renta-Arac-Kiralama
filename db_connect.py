a5c43e7fb06f62ef83503bc31c12052e14cb1723
erensahan99 <eren.sahan99@gmail.com>
2019-08-08 06:18:23 +0300

    database config

--- db_connect.py
+++ db_connect.py
@@ -2,7 +2,7 @@
 import mysql.connector
 
 def connection():
-    conn = mysql.connector.connect(host="mysql://b70af944e93ac1:e2aed107@us-cdbr-iron-east-02.cleardb.net/heroku_78cf1cf050b4c37?reconnect=true",
+    conn = mysql.connector.connect(host="us-cdbr-iron-east-02.cleardb.net",
                             user = "b70af944e93ac1",
                             passwd= "e2aed107",
                             db = "heroku_78cf1cf050b4c37")
